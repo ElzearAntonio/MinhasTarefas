f3a0e4423598422ee1d8d849b75e21472ddcec88
Yago Bernardes Vieira <yagobernardes062@gmail.com>
2025-04-23 19:31:49 -0300

    add_tarefas

--- screens/tarefas_screen/tarefasscreen.py
+++ screens/tarefas_screen/tarefasscreen.py
@@ -1,5 +1,13 @@
 from kivymd.uix.screen import MDScreen
 from kivymd.uix.toolbar import MDTopAppBar
+from kivymd.uix.list import MDList ,OneLineListItem
 
 class TarefasScreen(MDScreen):
-    pass
+    def adicionar_tarefas(self):
+        tarefa = self.ids.addtarefa
+        texto_tarefa = self.ids.addtarefa.text.strip()
+
+        if texto_tarefa:
+            nova_tarefa = OneLineListItem(text = texto_tarefa)
+            self.ids.lista_tarefas.add_widget(nova_tarefa)
+            tarefa.text = ''
